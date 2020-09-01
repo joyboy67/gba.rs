6c49a6173fe6a947e3c71e8d9b4176ee9108194b
ジョイボーイ <69261996+joyboy67@users.noreply.github.com>
2020-09-01 19:14:25 +0200

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,30 @@
+const :u8 ZERO_FLAG_BYTE_POSITION = 7;
+const :u8 SUBTRACT_FLAG_BYTE_POSITION = 6;
+const :u8 HALF_CARRY_FLAG_BYTE_POSITION = 5;
+const :u8 CARRY_FLAG_BYTE_POSITION = 4;
+
+
+impl std::convert::From<FlagsRegister> for u8  {
+    fn from(flag: FlagsRegister) -> u8 {
+        (if flag.zero       { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION |
+        (if flag.subtract   { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POSITION |
+        (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION |
+        (if flag.carry      { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
+    }
+}
+
+impl std::convert::From<u8> for FlagsRegister {
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+
+        FlagsRegister {
+            zero,
+            subtract,
+            half_carry,
+            carry
+        }
+    }
+}
